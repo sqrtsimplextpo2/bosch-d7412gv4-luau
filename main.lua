1ef10e46012f634197fedf1901ef348adbb8fbe3
tom <73983554+sqrtsimplextpo2@users.noreply.github.com>
2025-05-12 09:01:20 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,33 @@
+--SDI Bus
+local sdiAddresses = {}
+
+-- Active zones
+local activeZones = {}
+
+-- who are sdi keypads
+for i,v in pairs(script.Parent.SDI:GetChildren()) do
+	table.insert(sdiAddresses,v.Name)
+end
+
+-- hi keypads
+for i,v in pairs(script.Parent.SDI:GetChildren()) do
+	local disp = v:FindFirstChild("Display")
+	local pzo = v:FindFirstChild("Piezo")
+	if v then
+		task.wait()
+		disp.Line1.Value = "THIS IS KP"..i
+		pzo.Chime:Play()
+	end
+end
+
+while true do
+	for i,pt in pairs(script.Parent.Points:GetChildren()) do
+		for j,dev in pairs(pt.Devices:GetChildren()) do
+			if dev.Alarm.Value == true then
+				print("In Alarm:")
+			end
+			print(dev.Name.."\n" and dev.Alarm.Value == true or dev.Name)
+		end
+	end
+	task.wait(1)
+end
